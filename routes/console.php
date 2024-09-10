124666fc7568d5b1852b9a696ecd0c7bde5604fe
Ugbanawaji Leonard Ekenekiso <senenerst@gmail.com>
2024-09-10 13:28:54 +0100

    update

--- routes/console.php
+++ routes/console.php
@@ -21,3 +21,28 @@ Artisan::command('pint:clean', function () {
     });
     $this->comment('Done!');
 })->describe('Reformat and remove unused import.');
+
+Artisan::command('one:insight', function () {
+    $this->comment('Run request against bankone');
+    $customer =  new \App\Services\BankOneClient();
+    return $customer->createCustomer([
+        "TransactionTrackingRef" => "66c9f5ae60f6fe01e78b573a",
+        "AccountOpeningTrackingRef" => "66c9f5ae60f6fe01e78b573a",
+        "ProductCode" =>"107",
+        "LastName" =>"Mary",
+        "OtherNames" =>"Genny",
+        "BVN" =>"22656024483",
+        "PhoneNo" =>"08103469132",
+        "PlaceOfBirth" =>"Bauchi",
+        "DateOfBirth" =>"2024-08-24",
+        "Address" => "No 31 Afolabi Street, allen Avenue, Ikeja ,Lagos",
+        "NextOfKinPhoneNo" =>"09032432322",
+        "NextOfKinName" =>"Counsel Okpabi",
+        "HasSufficientInfoOnAccountInfo" =>true,
+        "email" =>"counselokpabijs@gmail.com",
+        "Gender" =>"male",
+        "NationalIdentityNo" => "31303938831",
+        "AccountOfficerCode" => "3453"
+    ]);
+    $this->comment('Done!');
+})->describe('Run a request bankone create customer API.');
